757b893142e78be810eaabc9caeaff166f4f4b4c
MrSemerena <mrsemerena@gmail.com>
2022-12-11 14:27:29 -0600

    Project's Initial Phase
    The libraries has been added, components had been created, the routes stablished, palette defined and the components had been included on the app-components.html file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import {  FlexLayoutModule  } from '@angular/flex-layout';
+import {  FlexModule  } from '@angular/flex-layout/flex';
+import {  GridModule  } from '@angular/flex-layout/grid';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    CoreModule,
+    BrowserModule,
+    FlexLayoutModule,
+    FlexModule,
+    GridModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
